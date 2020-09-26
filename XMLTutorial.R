23a08d744e389e086f345129573ca19c76aeb57a
Emily Clark <71150009+emilymclark@users.noreply.github.com>
2020-09-25 19:19:24 -0500

    Taking text and putting into dataframe

--- XMLTutorial.R
+++ XMLTutorial.R
@@ -20,8 +20,16 @@ xmlSize(xmltop) #how many children in node, 18
 xmlName(xmltop[[17]]) #name of root's children, body
 xmlName(xmltop[[16]]) #front
 xmltop[[16]] #shows text w/in front
-xmlName(xmltop[[17]][[4]]) #name of section 4 w/in body
-xmltop[[17]][[4]] #text w/in sec. 4 of body
+xmlName(xmltop[[17]][[2]]) #name of section 2 w/in body
+
+# Create list
+TextList <- list()
 
 # Convert the input xml file to a data frame.
-df <- xmlToDataFrame(xml_1, c("abstract", "body", "reference"))
+for (i in test1){
+  body <- paste(xpathSApply(xml_1, '//body', xmlValue), collapse = "|")
+  abstract <- paste(xpathSApply(xml_1, '//abstract', xmlValue), collapse = "|")
+  references <- paste(xpathSApply(xml_1, '//ref', xmlValue), collapse = "|")
+  temp_df <- cbind.data.frame(abstract,body,references)
+  TextList[[length(TextList)+1]] <- temp_df
+}
