1dee2a707fe9fd88329b58cb77951c753556c094
Emily Clark <71150009+emilymclark@users.noreply.github.com>
2020-10-06 14:27:49 -0500

    I think it finally worked!

--- ProtoDataSetContinued.R
+++ ProtoDataSetContinued.R
@@ -10,22 +10,23 @@ pmcid_list <- as.list(data$pmcid)
 
 # Convert the input xml file to a data frame.
 TextList <- list()
-for (i in pmcid_list){
-  temp <- europepmc::epmc_ftxt(i)
+for (i in pmcid_list[1:10]){
+  tryCatch({
+    temp <- europepmc::epmc_ftxt(i)}, error=function(e){})
   xml_1 <- xmlParse(temp)
   body <- paste(xpathSApply(xml_1, '//body', xmlValue), collapse = "|")
   abstract <- paste(xpathSApply(xml_1, '//abstract', xmlValue), collapse = "|")
   references <- paste(xpathSApply(xml_1, '//ref', xmlValue), collapse = "|")
-  temp_df <- cbind.data.frame(abstract,body,references)
+  pmcid <- i
+  temp_df <- cbind.data.frame(pmcid,abstract,body,references)
   TextList[[length(TextList)+1]] <- temp_df
-  }
+}
 
-#extract row from original dataframe
-row <- subset(data1, pmcid =="PMC7372935")
+#Make data frame
+text_data <- do.call(rbind, TextList)
 
-#cbind with temp_df (text) and select key variables
-whole <- cbind(row, temp_df) %>%
+#combine data frames
+AllData <- text_data %>% inner_join(data, by = "pmcid") %>%
   select(pmid,pmcid,title,journalTitle,pubYear,pubType,citedByCount,
          abstract,body,references)
-
 
