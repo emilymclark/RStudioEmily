b224ab1859a186b825a2195cbdbdf1797f9e9aca
Emily Clark <71150009+emilymclark@users.noreply.github.com>
2020-11-22 23:47:20 -0600

    Almost done with function

---
+++ UpdatedSearch1122.R
@@ -0,0 +1,86 @@
+
+# PMC Full Text Download --------------------------------------------------
+# Created for public health ethics research project Graham & Clark 
+
+
+# Pre-Flight --------------------------------------------------------------
+
+#Load libraries 
+library(dplyr)
+library(XML)
+library(europepmc)
+library(purrr)
+library(rentrez)
+library(tidytext)
+
+# Set Entrez API key (https://ncbiinsights.ncbi.nlm.nih.gov/2017/11/02/new-api-keys-for-the-e-utilities/)
+key = ""
+set_entrez_key(key)
+
+# Functions ---------------------------------------------------------------
+# Function searches Europe PMC per query (set limit to? )
+get_opioid_data <- function(x){
+  europepmc::epmc_search(query = paste0('(opioid) AND PUB_YEAR:',x, ' AND IN_EPMC:y'),
+                         limit = 10, sort = "cited")
+}
+
+#Function uses Enterz API key to extract and parse full text 
+extract_text <- function(x){
+  file <- entrez_fetch("pmc", id = x,  rettype="xml",parsed = FALSE)
+  xml_1 <- xmlParse(file)
+  body <- paste(xpathSApply(xml_1, '//body', xmlValue), collapse = "|")
+  abstract <- paste(xpathSApply(xml_1, '//abstract', xmlValue), collapse = "|")
+  references <- paste(xpathSApply(xml_1, '//ref', xmlValue), collapse = "|")
+  pmcid <- x
+  Sys.sleep(1)
+  cbind.data.frame(pmcid,abstract,body,references)
+}
+
+# Data Collection ---------------------------------------------------------
+
+# Set PMC Year range 
+years <- c(2005:2019)
+
+# Get top cited opioid articles per year 
+data <- map_df(years,get_opioid_data)
+
+# Get available text data for top cited opioid articles
+text_data <- map_df(data$pmcid, extract_text)
+
+# Count N of articles with full body text available
+#Should this be "text_data" not "text"? - tried and worked
+#idk if this is right since I probably wasn't supposed to change anything
+text_data %>% filter(nchar(body)>1)%>% nrow()
+
+#but then what's this line for?
+text_data <- text
+
+#combine data frames
+AllData <- text_data %>% inner_join(data, by = "pmcid") %>%
+  select(pmid,pmcid,title,journalTitle,pubYear,firstPublicationDate,pubType,citedByCount,
+         abstract,body,references)
+
+#Filter data to include only rows that have both abstract and body text data. 
+AllData2 <- subset(AllData,AllData$abstract>0 & AllData$body>0)
+
+#Write a function that:
+SentTok <- function(x){
+  #Tokenizes by sentence 
+sent <- unnest_tokens(AllData2, sentences, body, token = "sentences")
+  #Counts the total number of sentences
+  #Is this per article or total sentences?
+  #Since total sentences is just the total # of rows, I'll go with per article
+countperart <- sent %>%
+  group_by(pmcid) %>%
+  tally()
+sentcount <- merge(sent, countperart, by = c("pmcid","pmcid"))
+
+  #Evaluates each sentence for statements of obligation 
+ob_sent <- subset(sentcount, str_detect(sent$sentences,"oblig|responsib|need|must|duty|duties|account|bound| owe|require|liability")==TRUE)
+  #Classifies obligation sentences: patient vs. society 
+ob_sent$topat <- ifelse(str_detect(ob_sent$sentences,"patient|treat|pain|suffer|relief|relieve")==TRUE,"patient",0)
+ob_sent$tosoc <- ifelse(str_detect(ob_sent$sentences,"societ|communit|public|law")==TRUE,"tosoc",0)
+  #Counts number of patient obligation sentences
+topat_count <- ob_sent$topat %>% count("patient")
+  #Counts number of society obligation sentences
+  #Returns a df row: n_sent, n_pat, n_soc
