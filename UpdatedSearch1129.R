03717d3b461500b4f282272fb8c2d1cd2a728cfa
Emily Clark <71150009+emilymclark@users.noreply.github.com>
2020-11-29 18:42:11 -0600

    Completed function, now need to test on abstract and body

---
+++ UpdatedSearch1129.R
@@ -0,0 +1,124 @@
+
+# PMC Full Text Download --------------------------------------------------
+# Created for public health ethics research project Graham & Clark 
+
+
+# Pre-Flight --------------------------------------------------------------
+
+#Load libraries 
+library(dplyr)
+library(XML)
+library(europepmc)
+library(purrr)
+library(rentrez)
+library(tidytext)
+library(quanteda)
+library(tidyselect)
+library(tidyr)
+
+# Set Entrez API key (https://ncbiinsights.ncbi.nlm.nih.gov/2017/11/02/new-api-keys-for-the-e-utilities/)
+key = ""
+set_entrez_key(key)
+
+# Functions ---------------------------------------------------------------
+# Function searches Europe PMC per query (set limit to? )
+get_opioid_data <- function(x){
+  europepmc::epmc_search(query = paste0('(opioid) AND PUB_YEAR:',x, ' AND IN_EPMC:y'),
+                         limit = 10, sort = "cited")
+}
+
+#Function uses Enterz API key to extract and parse full text 
+extract_text <- function(x){
+  file <- entrez_fetch("pmc", id = x,  rettype="xml",parsed = FALSE)
+  xml_1 <- xmlParse(file)
+  body <- paste(xpathSApply(xml_1, '//body', xmlValue), collapse = "|")
+  abstract <- paste(xpathSApply(xml_1, '//abstract', xmlValue), collapse = "|")
+  references <- paste(xpathSApply(xml_1, '//ref', xmlValue), collapse = "|")
+  pmcid <- x
+  Sys.sleep(1)
+  cbind.data.frame(pmcid,abstract,body,references)
+}
+
+# Data Collection ---------------------------------------------------------
+
+# Set PMC Year range 
+years <- c(2005:2019)
+
+# Get top cited opioid articles per year 
+data <- map_df(years,get_opioid_data)
+
+# Get available text data for top cited opioid articles
+text_data <- map_df(data$pmcid, extract_text)
+
+# Count N of articles with full body text available
+#Should this be "text_data" not "text"? - tried and worked
+#idk if this is right since I probably wasn't supposed to change anything
+text_data %>% filter(nchar(body)>1)%>% nrow()
+
+#but then what's this line for?
+text_data <- text
+
+#combine data frames
+AllData <- text_data %>% inner_join(data, by = "pmcid") %>%
+  select(pmid,pmcid,title,journalTitle,pubYear,firstPublicationDate,pubType,citedByCount,
+         abstract,body,references)
+
+#Filter data to include only rows that have both abstract and body text data. 
+AllData2 <- subset(AllData,AllData$abstract>0 & AllData$body>0)
+y
+#Write a function that:
+SentTok <- function(x){
+    #Tokenizes by sentence 
+    #Need to figure out how to apply to any character column
+  sent <- unnest_tokens(AllData2, sentences, ..., token = "sentences")
+    #length(x)?
+    #nrow(sent)
+      #shows length of df
+    #n_sent <- length(sent)
+      #Creates value showing number of variables
+    #Creates value with df length (# of tokens)
+      #n_sent <- sent %>% nrow()
+#Counts the total number of sentences
+  #Is this per article or total sentences?
+    #per article
+      #countperart <- sent %>%
+       #group_by(pmcid) %>%
+       #tally()
+    #Merge sent count per article w/ df
+      #sentcount <- merge(sent, countperart, by = c("pmcid","pmcid"))
+  #Evaluates each sentence for statements of obligation 
+ob_sent <- filter(sent, str_detect(sent$sentences,"oblig|responsib|need|must|duty|duties|account|bound| owe|require|liability")==TRUE)
+  #Classifies obligation sentences: patient vs. society 
+ob_sent$topat <- ifelse(str_detect(ob_sent$sentences,"patient|treat|pain|suffer|relief|relieve")==TRUE,1,0)
+ob_sent$tosoc <- ifelse(str_detect(ob_sent$sentences,"societ|communit|public|law")==TRUE,1,0)
+    #Creates table with count 
+    #This does that but not efficiently 
+      #stats <- count(ob_sent, topat, tosoc)
+    #This creates readable output in console
+      #table(ob_sent$topat,ob_sent$tosoc)
+  #Creates single integer dfs that count totals
+n_sent <- sent %>% tally()
+n_topat <- ob_sent %>% tally(topat)
+n_tosoc <- ob_sent %>% tally(tosoc)
+n_both <- ob_sent %>% tally(topat & tosoc)
+  #bind all into one table
+Totals <- cbind(n_sent,n_topat,n_tosoc,n_both)
+  #rename columns
+colnames(Totals) <- c("n_sent", "n_pat", "n_soc", "n_both")
+#Returns a df row: n_sent, n_pat, n_soc
+}
+#Seems that some sentences may have multiple obligations if I try to run all together 
+#ob_sent$obto <- ifelse(str_detect(ob_sent$sentences, "patient|treat|pain|suffer|relief|relieve")==TRUE,"patient", ifelse(str_detect(ob_sent$sentences,"societ|communit|public|law")==TRUE,"society",0))
+
+  
+#This works to count but doesn't create df 
+table(ob_sent$topat,ob_sent$tosoc)
+tab <- count(ob_sent$tosoc == 1)
+
+#counts but table is weird, better ways for sure
+  #stats <- count(ob_sent, topat, tosoc)
+
+wtf <- ob_sent %>%
+  group_by(topat,tosoc) %>%
+  tally()
+count(ob_sent$tosoc,"1")
